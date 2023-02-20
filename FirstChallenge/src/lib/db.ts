226fe52f69a082198237b66113f6f902fce92dcf
MosviRs <mosvi_randy@hotmail.com>
2023-02-20 13:27:26 -0600

    delete console

--- FirstChallenge/src/lib/db.ts
+++ FirstChallenge/src/lib/db.ts
@@ -1,6 +1,5 @@
 import { Sequelize } from "sequelize";
 import { DATABASE } from "../config/constants";
-console.log("DATABASE", DATABASE);
 export const sequelize = new Sequelize(
   DATABASE.database,
   DATABASE.user,
