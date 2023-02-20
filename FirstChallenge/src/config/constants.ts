c1d5fcab89ca13982a7b4cd1bf2c41ea483d406a
MosviRs <mosvi_randy@hotmail.com>
2023-02-20 13:22:33 -0600

    set up docker compose

--- FirstChallenge/src/config/constants.ts
+++ FirstChallenge/src/config/constants.ts
@@ -1,11 +1,9 @@
 const DATABASE = {
-    host: process.env.HOST || 'localhost',
-    user: process.env.USERDB || 'postgres',
-    password: process.env.PASSWORD || "root",
-    database: process.env.DATABASE || "bienes",
+  host: process.env.HOST || "localhost",
+  user: process.env.USERDB || "postgres",
+  password: process.env.PASSWORD || "root",
+  database: process.env.DATABASE || "bienes",
+  port: process.env.PORTDB || 5432,
 };
 const JWT_SECRET_KEY = process.env.JWT_SECRET || "mysecret";
-export {
-    DATABASE,
-    JWT_SECRET_KEY
-}
+export { DATABASE, JWT_SECRET_KEY };
--- FirstChallenge/src/lib/db.ts
+++ FirstChallenge/src/lib/db.ts
@@ -1,6 +1,13 @@
-import { Sequelize } from 'sequelize';
-import {DATABASE} from '../config/constants';
-export const sequelize = new Sequelize(DATABASE.database,DATABASE.user,DATABASE.password,{
+import { Sequelize } from "sequelize";
+import { DATABASE } from "../config/constants";
+console.log("DATABASE", DATABASE);
+export const sequelize = new Sequelize(
+  DATABASE.database,
+  DATABASE.user,
+  DATABASE.password,
+  {
     host: DATABASE.host,
-    dialect: 'postgres'
-});
+    dialect: "postgres",
+    port: DATABASE.port as number,
+  }
+);
